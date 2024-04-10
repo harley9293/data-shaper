d5751fed80cd7bcdbdeeda14f298f0c31cf06a3c
harley9293 <xuhanlin2012@gmail.com>
2024-04-10 14:42:42 +0800

    fix

--- internal/converter/converter_test.go
+++ internal/converter/converter_test.go
@@ -1,10 +1,15 @@
 package converter
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestProtoToExcel(t *testing.T) {
 	err := ProtoToExcel("../../test/test.proto", "")
 	if err != nil {
 		t.Errorf("ProtoToExcel() error = %v", err)
 	}
+
+	_ = os.Remove("测试配置.xlsx")
 }
