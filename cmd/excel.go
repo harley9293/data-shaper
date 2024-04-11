3624a65396e38fa6e9a61d7ccd80ca7de12031a7
harley9293 <xuhanlin2012@gmail.com>
2024-04-11 11:09:32 +0800

    refator

--- cmd/excel.go
+++ cmd/excel.go
@@ -7,44 +7,51 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var create bool
-var export bool
-var excelCmd = &cobra.Command{
-	Use:   "excel",
-	Short: "excel file conversion and generation",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		if create == export {
-			return errors.New("either 'create' or 'export' must be specified, but not both")
-		}
+func ExcelCmd() *cobra.Command {
+	var create bool
+	var export bool
+	cmd := &cobra.Command{
+		Use:   "excel",
+		Short: "excel file conversion and generation",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if create == export {
+				return errors.New("either 'create' or 'export' must be specified, but not both")
+			}
 
-		return nil
-	},
+			return nil
+		},
+	}
+	cmd.DisableFlagsInUseLine = true
+
+	return cmd
 }
 
-var protoPath string
-var excelPath string
-var createCmd = &cobra.Command{
-	Use:   "create",
-	Short: "Create an Excel file from a proto file",
-	Long:  `Create an Excel file from a proto file.`,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		// 调用转换逻辑
-		err := converter.ProtoToExcel(protoPath, excelPath)
-		if err != nil {
-			return err
-		}
-		fmt.Println("Excel file created successfully at", excelPath)
-		return nil
-	},
+func CreateCmd() *cobra.Command {
+	var protoPath string
+	var excelPath string
+	cmd := &cobra.Command{
+		Use:   "create",
+		Short: "Create an Excel file from a proto file",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			err := converter.ProtoToExcel(protoPath, excelPath)
+			if err != nil {
+				return err
+			}
+			fmt.Println("Excel file created successfully at", excelPath)
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVar(&protoPath, "proto", "", "Path to the proto file")
+	cmd.MarkFlagRequired("proto")
+	cmd.Flags().StringVar(&excelPath, "excel", "", "Path for the resulting Excel file")
+	cmd.MarkFlagRequired("excel")
+
+	return cmd
 }
 
 func init() {
-	createCmd.Flags().StringVar(&protoPath, "proto", "", "Path to the proto file")
-	createCmd.MarkFlagRequired("proto")
-	createCmd.Flags().StringVar(&excelPath, "excel", "", "Path for the resulting Excel file")
-	createCmd.MarkFlagRequired("excel")
-	excelCmd.AddCommand(createCmd)
-
-	excelCmd.DisableFlagsInUseLine = true
+	excelCmd := ExcelCmd()
+	excelCmd.AddCommand(CreateCmd())
 	rootCmd.AddCommand(excelCmd)
 }
